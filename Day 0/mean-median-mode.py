241a7ca4cc7b6bdb78856392bcf3931b603ffe3a
dhruv970331 <dhruveshmalhotra186@gmail.com>
2020-01-27 22:56:26 +0530

    mean-median-mode done

--- Day 0/mean-median-mode.py
+++ Day 0/mean-median-mode.py
@@ -4,16 +4,15 @@ from collections import Counter
 n = int(input())
 arr = [int(i) for i in input().split()]
 
-# print(arr)
-
 # Mean
 print(sum(arr)/n)
 s = sorted(arr)
 
-#Median
+# Median
 print((s[n//2]+s[(n//2)-1])/2)
 
 counter = Counter(arr)
-# f = filter(lambda x,y:x if y==min(counter.values()),counter.items())
+f = filter(lambda x: counter.get(x) >= max(counter.values()), counter.keys())
 # Mode
-print(min(counter))
+
+print(min(f))
